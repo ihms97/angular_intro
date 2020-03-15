c7c4d44c8a969d0aff47fe70bc8c9afdbd36c34f
imeier <ian.meier@netbyteoss.cl>
2020-03-15 09:20:46 -0300

    Se arregla error con conexión bd (error build/index.js).

--- server/build/database.js
+++ server/build/database.js
@@ -4,13 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mysql_1 = __importDefault(require("mysql"));
-`enter code here`;
 const keys_1 = __importDefault(require("./keys"));
 const pool = mysql_1.default.createPool(keys_1.default.database);
 pool.getConnection((err, connection) => {
-    if (err)
-        throw err;
-    connection.release();
-    console.log('DB is connected');
+    //     if (err) throw err; connection.release();
+    //     console.log('DB is connected');
+    if (err) {
+        console.log('Error in database connection: ', err.message, err.stack);
+    }
+    else {
+        connection.release();
+        console.log('DB is connected');
+    }
 });
 exports.default = pool;
--- server/src/database.ts
+++ server/src/database.ts
@@ -1,13 +1,19 @@
-import mysql from 'mysql';`enter code here`
+import mysql from 'mysql';
 import keys from './keys';
 
 const pool = mysql.createPool(keys.database);
 
 
 pool.getConnection((err, connection) => {
-     if (err) throw err; connection.release();
-     console.log('DB is connected');
+//     if (err) throw err; connection.release();
+//     console.log('DB is connected');
 
+     if (err) {
+       console.log('Error in database connection: ', err.message, err.stack);
+     } else {
+       connection.release();
+       console.log('DB is connected');
+     }
 });
 
 export default pool;
