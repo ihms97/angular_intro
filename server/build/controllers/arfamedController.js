229466c792c988daf976a2ae99c6facd0097a444
imeier <ian.meier@netbyteoss.cl>
2020-03-16 22:04:47 -0300

    Se completan las diferentes apis en los controladores para concretar el crud.

--- server/build/controllers/arfamedController.js
+++ server/build/controllers/arfamedController.js
@@ -1,14 +1,83 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = __importDefault(require("../database"));
 class ArfamedController {
-    index(req, res) {
-        database_1.default.query('DESCRIBE previsiones');
-        res.json('Arfamed desde arfamedController');
-        // res.send('Enviado desde clase IndexController hacia indexRoutes.ts');
+    consult(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield database_1.default.query('select * from profesional where cod_prof = ' + req.params.id, (err, result) => {
+                if (err) {
+                    throw err;
+                }
+                else {
+                    if (!result.length) {
+                        res.json({ message: 'No existe el usuario solicitado' });
+                    }
+                    else {
+                        res.json(result);
+                    }
+                }
+            });
+        });
+    }
+    list(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield database_1.default.query('select * from profesional', (err, result) => {
+                if (err) {
+                    throw err;
+                }
+                else {
+                    res.json(result);
+                }
+            });
+        });
+    }
+    create(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield database_1.default.query('insert into profesional set ?', [req.body], (err, result) => {
+                if (err) {
+                    throw err;
+                }
+                else {
+                    res.json({ message: 'Profesional Guardado Satisfactoriamente' });
+                }
+            });
+        });
+    }
+    delete(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield database_1.default.query('delete from profesional where cod_prof = ' + req.params.id, (err, result) => {
+                if (err) {
+                    throw err;
+                }
+                else {
+                    res.json('Profesional eliminado correctamente.');
+                }
+            });
+        });
+    }
+    update(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield database_1.default.query('update profesional set ? where cod_prof = ?', [req.body, req.params.id], (err, result) => {
+                if (err) {
+                    throw err;
+                }
+                else {
+                    res.json({ message: "The game was Updated" });
+                }
+            });
+        });
     }
 }
 exports.arfamedController = new ArfamedController();
--- server/src/controllers/arfamedController.ts
+++ server/src/controllers/arfamedController.ts
@@ -3,11 +3,60 @@ import {Request, Response} from 'express';
 import pool from '../database';
 
 class ArfamedController {
-  public index (req: Request, res: Response) {
-    pool.query('DESCRIBE previsiones');
-    res.json('Arfamed desde arfamedController');
-    // res.send('Enviado desde clase IndexController hacia indexRoutes.ts');
+  public async consult (req: Request, res: Response): Promise<any> {
+    await pool.query('select * from profesional where cod_prof = ' + req.params.id, (err, result) => {
+      if (err) {
+        throw err;
+      } else {
+        if (!result.length) {
+          res.json({message: 'No existe el usuario solicitado'});
+        } else {
+          res.json(result);
+        }
+      }
+    });
   }
+
+  public async list (req: Request, res: Response): Promise<any> {
+    await pool.query('select * from profesional', (err, result) => {
+      if (err) {
+        throw err;
+      } else {
+        res.json(result);
+      }
+    });
+  }
+
+  public async create (req: Request, res: Response): Promise<any> {
+    await pool.query('insert into profesional set ?', [req.body], (err, result) => {
+      if (err) {
+        throw err;
+      } else {
+        res.json({message: 'Profesional Guardado Satisfactoriamente'});
+      }
+    });
+  }
+
+  public async delete (req: Request, res: Response): Promise<any> {
+    await pool.query('delete from profesional where cod_prof = ' + req.params.id, (err, result) => {
+      if (err) {
+        throw err;
+      } else {
+        res.json('Profesional eliminado correctamente.');
+      }
+    });
+  }
+
+  public async update (req: Request, res: Response): Promise<void> {
+    await pool.query('update profesional set ? where cod_prof = ?', [req.body, req.params.id], (err, result) => {
+      if (err) {
+        throw err;
+      } else {
+        res.json({ message: "The game was Updated" });
+      }
+    })
+  }
+
 }
 
 export const arfamedController = new ArfamedController();
